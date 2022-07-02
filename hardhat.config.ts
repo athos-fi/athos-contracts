9c43ffd110c24def081d042a989af002e16f9f23
Tommy <tommy@athos.finance>
2022-07-02 17:31:29 +0000

    chore: add Hardhat

---
+++ hardhat.config.ts
@@ -0,0 +1,41 @@
+import "@nomiclabs/hardhat-waffle";
+import "@openzeppelin/hardhat-upgrades";
+import "@typechain/hardhat";
+import { HardhatUserConfig } from "hardhat/types";
+
+const config: HardhatUserConfig = {
+  paths: {
+    cache: "hh_cache",
+    artifacts: "hh_out",
+    sources: "src",
+  },
+  networks: {},
+  solidity: {
+    compilers: [
+      // For WETH9
+      {
+        version: "0.4.19",
+        settings: {
+          optimizer: {
+            enabled: false,
+            runs: 200,
+          },
+        },
+      },
+      {
+        version: "0.8.15",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 999999,
+          },
+        },
+      },
+    ],
+  },
+  typechain: {
+    outDir: "typechain",
+  },
+};
+
+export default config;
