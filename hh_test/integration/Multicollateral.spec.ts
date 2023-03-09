8e5875214c967d82bdb9e3ca56a43af60df88780
Tommy <tommy@athos.finance>
2023-03-09 04:29:26 +0000

    test: add debt change cases for multi-collateral

--- hh_test/integration/Multicollateral.spec.ts
+++ hh_test/integration/Multicollateral.spec.ts
@@ -1,5 +1,7 @@
+import { Duration } from "luxon";
 import { ethers } from "hardhat";
 import { expect } from "chai";
+import { BigNumberish } from "ethers";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/dist/src/signer-with-address";
 
 import {
@@ -8,14 +10,45 @@ import {
   uint256Max,
 } from "../utilities";
 import { deployAthosStack, DeployedStack } from "../utilities/init";
-import { getBlockDateTime } from "../utilities/timeTravel";
+import {
+  getBlockDateTime,
+  setNextBlockTimestamp,
+} from "../utilities/timeTravel";
+
+import { IDebtSystem } from "../../typechain";
 
 const { formatBytes32String } = ethers.utils;
+
+enum CollateralType {
+  ATH,
+  WBTC,
+}
 
 describe("Integration | Multicollateral", function () {
   let deployer: SignerWithAddress, alice: SignerWithAddress;
 
   let stack: DeployedStack;
+
+  const assertDebtBalance = async (
+    user: string,
+    collateral: CollateralType,
+    amount: BigNumberish
+  ) => {
+    let debtSystem: IDebtSystem;
+    switch (collateral) {
+      case CollateralType.ATH:
+        debtSystem = stack.collaterals.ath.debtSystem;
+        break;
+      case CollateralType.WBTC:
+        debtSystem = stack.collaterals.wbtc.debtSystem;
+        break;
+      default:
+        throw new Error("Unknown collateral type");
+    }
+    expect((await debtSystem.GetUserDebtBalanceInUsd(user))[0]).to.equal(
+      amount
+    );
+  };
 
   beforeEach(async function () {
     [deployer, alice] = await ethers.getSigners();
@@ -189,20 +222,8 @@ describe("Integration | Multicollateral", function () {
 
     it("debt from different collaterals are separated", async function () {
       // Alice has no debt on either side to begin with
-      expect(
-        (
-          await stack.collaterals.ath.debtSystem.GetUserDebtBalanceInUsd(
-            alice.address
-          )
-        )[0]
-      ).to.equal(0);
-      expect(
-        (
-          await stack.collaterals.wbtc.debtSystem.GetUserDebtBalanceInUsd(
-            alice.address
-          )
-        )[0]
-      ).to.equal(0);
+      await assertDebtBalance(alice.address, CollateralType.ATH, 0);
+      await assertDebtBalance(alice.address, CollateralType.WBTC, 0);
 
       // Alice builds 10 athUSD from ATH
       await stack.collaterals.ath.buildBurnSystem.connect(alice).BuildAsset(
@@ -210,20 +231,12 @@ describe("Integration | Multicollateral", function () {
       );
 
       // ATH debt increases
-      expect(
-        (
-          await stack.collaterals.ath.debtSystem.GetUserDebtBalanceInUsd(
-            alice.address
-          )
-        )[0]
-      ).to.equal(expandTo18Decimals(10));
-      expect(
-        (
-          await stack.collaterals.wbtc.debtSystem.GetUserDebtBalanceInUsd(
-            alice.address
-          )
-        )[0]
-      ).to.equal(0);
+      await assertDebtBalance(
+        alice.address,
+        CollateralType.ATH,
+        expandTo18Decimals(10)
+      );
+      await assertDebtBalance(alice.address, CollateralType.WBTC, 0);
 
       // Alice builds 20 athUSD from WBTC
       await stack.collaterals.wbtc.buildBurnSystem.connect(alice).BuildAsset(
@@ -231,20 +244,192 @@ describe("Integration | Multicollateral", function () {
       );
 
       // WBTC debt increases
-      expect(
-        (
-          await stack.collaterals.ath.debtSystem.GetUserDebtBalanceInUsd(
-            alice.address
-          )
-        )[0]
-      ).to.equal(expandTo18Decimals(10));
-      expect(
-        (
-          await stack.collaterals.wbtc.debtSystem.GetUserDebtBalanceInUsd(
-            alice.address
-          )
-        )[0]
-      ).to.equal(expandTo18Decimals(20));
+      await assertDebtBalance(
+        alice.address,
+        CollateralType.ATH,
+        expandTo18Decimals(10)
+      );
+      await assertDebtBalance(
+        alice.address,
+        CollateralType.WBTC,
+        expandTo18Decimals(20)
+      );
+    });
+  });
+
+  describe("Debt Changes", function () {
+    const settlementDelay: Duration = Duration.fromObject({ minutes: 1 });
+
+    // Helper functions
+    const setLbtcPrice = async (price: number): Promise<void> => {
+      await stack.abtcOracle.connect(deployer).setPrice(
+        expandTo8Decimals(price) // price
+      );
+    };
+    const passSettlementDelay = async (): Promise<void> => {
+      await setNextBlockTimestamp(
+        ethers.provider,
+        (await getBlockDateTime(ethers.provider)).plus(settlementDelay)
+      );
+    };
+    const settleTrade = (entryId: number): Promise<any> => {
+      return stack.exchangeSystem.connect(deployer).settle(
+        entryId // pendingExchangeEntryId
+      );
+    };
+    const settleTradeWithDelay = async (entryId: number): Promise<any> => {
+      await passSettlementDelay();
+      await settleTrade(entryId);
+    };
+
+    beforeEach(async function () {
+      // Alice stakes 10,000 ATH and 1 WBTC
+      await stack.collaterals.ath.collateralSystem.connect(alice).Collateral(
+        formatBytes32String("ATH"), // _currency
+        expandTo18Decimals(10_000) // _amount
+      );
+      await stack.collaterals.wbtc.collateralSystem.connect(alice).Collateral(
+        formatBytes32String("WBTC"), // _currency
+        expandTo8Decimals(1) // _amount
+      );
+
+      // Alice builds 10 athUSD from ATH and 20 athUSD from WBTC
+      await stack.collaterals.ath.buildBurnSystem.connect(alice).BuildAsset(
+        expandTo18Decimals(10) // amount
+      );
+      await stack.collaterals.wbtc.buildBurnSystem.connect(alice).BuildAsset(
+        expandTo18Decimals(20) // amount
+      );
+
+      // Set settlement delay
+      await stack.config.connect(deployer).setUint(
+        formatBytes32String("TradeSettlementDelay"), // key
+        settlementDelay.as("seconds")
+      );
+      await stack.config.connect(deployer).setUint(
+        formatBytes32String("TradeRevertDelay"), // key
+        Duration.fromObject({ years: 1 }).as("seconds")
+      );
+
+      // Set lBTC price to $20,000
+      await setLbtcPrice(20_000);
+
+      // Alice exchanges 10 athUSD for 0.0005 lBTC
+      await stack.exchangeSystem.connect(alice).exchange(
+        formatBytes32String("athUSD"), // sourceKey
+        expandTo18Decimals(10), // sourceAmount
+        alice.address, // destAddr
+        formatBytes32String("aBTC") // destKey
+      );
+      await settleTradeWithDelay(1);
+    });
+
+    it("exchanging should not affect debt amounts", async function () {
+      await assertDebtBalance(
+        alice.address,
+        CollateralType.ATH,
+        expandTo18Decimals(10)
+      );
+      await assertDebtBalance(
+        alice.address,
+        CollateralType.WBTC,
+        expandTo18Decimals(20)
+      );
+    });
+
+    it("debt should increase proportionally across collaterals", async function () {
+      // Set lBTC price to $80,000
+      await setLbtcPrice(80_000);
+
+      // Total debt now:
+      //   20 lUSD
+      //   0.0005 lBTC = 0.0005 * 80,000 = 40 lUSD
+      //   Total = 20 + 40 = 60 lUSD
+      //
+      // Per collateral:
+      //   ATH: 20 lUSD
+      //   WBTC: 40 lUSD
+      await assertDebtBalance(
+        alice.address,
+        CollateralType.ATH,
+        expandTo18Decimals(20)
+      );
+      await assertDebtBalance(
+        alice.address,
+        CollateralType.WBTC,
+        expandTo18Decimals(40)
+      );
+    });
+
+    it("debt should decrease proportionally across collaterals", async function () {
+      // Set lBTC price to $2,000
+      await setLbtcPrice(2_000);
+
+      // Total debt now:
+      //   20 lUSD
+      //   0.0005 lBTC = 0.0005 * 2,000 = 1 lUSD
+      //   Total = 20 + 1 = 21 lUSD
+      //
+      // Per collateral:
+      //   ATH: 7 lUSD
+      //   WBTC: 14 lUSD
+      await assertDebtBalance(
+        alice.address,
+        CollateralType.ATH,
+        expandTo18Decimals(7)
+      );
+      await assertDebtBalance(
+        alice.address,
+        CollateralType.WBTC,
+        expandTo18Decimals(14)
+      );
+    });
+
+    it("burning debt works after debt changes from exchange", async function () {
+      // Set lBTC price to $2,000 so that debt per collateral:
+      //   ATH: 7 lUSD
+      //   WBTC: 14 lUSD
+      await setLbtcPrice(2_000);
+      await assertDebtBalance(
+        alice.address,
+        CollateralType.ATH,
+        expandTo18Decimals(7)
+      );
+      await assertDebtBalance(
+        alice.address,
+        CollateralType.WBTC,
+        expandTo18Decimals(14)
+      );
+
+      // Alice burns 2 lUSD of ATH debt
+      await stack.collaterals.ath.buildBurnSystem
+        .connect(alice)
+        .BurnAsset(expandTo18Decimals(2));
+      await assertDebtBalance(
+        alice.address,
+        CollateralType.ATH,
+        expandTo18Decimals(5)
+      );
+      await assertDebtBalance(
+        alice.address,
+        CollateralType.WBTC,
+        expandTo18Decimals(14)
+      );
+
+      // Alice burns 4 lUSD of WBTC debt
+      await stack.collaterals.wbtc.buildBurnSystem
+        .connect(alice)
+        .BurnAsset(expandTo18Decimals(4));
+      await assertDebtBalance(
+        alice.address,
+        CollateralType.ATH,
+        expandTo18Decimals(5)
+      );
+      await assertDebtBalance(
+        alice.address,
+        CollateralType.WBTC,
+        expandTo18Decimals(10)
+      );
     });
   });
 });
