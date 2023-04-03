447dd5fed69e3a7e68dcd74a8a2b5fcd6b02a66c
venusss123 <venus@athos.finance>
2023-04-03 06:48:09 +0000

    feat: update mockRewardSytem in athos format

--- src/mock/MockRewardSystem.sol
+++ src/mock/MockRewardSystem.sol
@@ -1,12 +1,13 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.7.6;
+pragma solidity =0.8.15;
 
-import "@openzeppelin/contracts-upgradeable/cryptography/ECDSAUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/math/SafeMathUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/cryptography/ECDSAUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/math/SafeMathUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
-import "../interfaces/ILnCollateralSystem.sol";
-import "../interfaces/ILnRewardLocker.sol";
-import "../upgradeable/LnAdminUpgradeable.sol";
+import "../interfaces/ICollateralSystem.sol";
+import "../interfaces/IRewardLocker.sol";
+
 
 /**
  * @title MockRewardSystem
@@ -14,7 +15,7 @@ import "../upgradeable/LnAdminUpgradeable.sol";
  * @dev Exactly the same as `LnRewardSystem` except `PERIOD_LENGTH` and `STAKING_REWARD_LOCK_PERIOD`
  * for use in internal development environment.
  */
-contract MockRewardSystem is LnAdminUpgradeable {
+contract MockRewardSystem is OwnableUpgradeable {
     using ECDSAUpgradeable for bytes32;
     using SafeMathUpgradeable for uint256;
 
@@ -27,8 +28,8 @@ contract MockRewardSystem is LnAdminUpgradeable {
     mapping(address => uint256) public userLastClaimPeriodIds;
 
     IERC20Upgradeable public lusd;
-    ILnCollateralSystem public collateralSystem;
-    ILnRewardLocker public rewardLocker;
+    ICollateralSystem public collateralSystem;
+    IRewardLocker public rewardLocker;
 
     bytes32 public DOMAIN_SEPARATOR; // For EIP-712
 
@@ -41,35 +42,30 @@ contract MockRewardSystem is LnAdminUpgradeable {
     uint256 public constant STAKING_REWARD_LOCK_PERIOD = 30 minutes;
 
     function getCurrentPeriodId() public view returns (uint256) {
-        require(block.timestamp >= firstPeriodStartTime, "LnRewardSystem: first period not started");
+        require(block.timestamp >= firstPeriodStartTime, "RewardSystem: first period not started");
         return (block.timestamp - firstPeriodStartTime) / PERIOD_LENGTH + 1; // No SafeMath needed
     }
 
     function getPeriodStartTime(uint256 periodId) public view returns (uint256) {
-        require(periodId > 0, "LnRewardSystem: period ID must be positive");
+        require(periodId > 0, "RewardSystem: period ID must be positive");
         return firstPeriodStartTime.add(periodId.sub(1).mul(PERIOD_LENGTH));
     }
 
     function getPeriodEndTime(uint256 periodId) public view returns (uint256) {
-        require(periodId > 0, "LnRewardSystem: period ID must be positive");
+        require(periodId > 0, "RewardSystem: period ID must be positive");
         return firstPeriodStartTime.add(periodId.mul(PERIOD_LENGTH));
     }
 
-    function __LnRewardSystem_init(
+    function __RewardSystem_init(
         uint256 _firstPeriodStartTime,
         address _rewardSigner,
         address _lusdAddress,
         address _collateralSystemAddress,
-        address _rewardLockerAddress,
-        address _admin
+        address _rewardLockerAddress
     ) public initializer {
-        __LnAdminUpgradeable_init(_admin);
+        __Ownable_init();
 
-        /**
-         * The next line is commented out to make migration from Ethereum to Binance Smart
-         * chain possible.
-         */
-        // require(block.timestamp < _firstPeriodStartTime + PERIOD_LENGTH, "LnRewardSystem: first period already ended");
+        require(block.timestamp < _firstPeriodStartTime + PERIOD_LENGTH, "RewardSystem: first period already ended");
 
         firstPeriodStartTime = _firstPeriodStartTime;
 
@@ -77,11 +73,11 @@ contract MockRewardSystem is LnAdminUpgradeable {
 
         require(
             _lusdAddress != address(0) && _collateralSystemAddress != address(0) && _rewardLockerAddress != address(0),
-            "LnRewardSystem: zero address"
+            "RewardSystem: zero address"
         );
         lusd = IERC20Upgradeable(_lusdAddress);
-        collateralSystem = ILnCollateralSystem(_collateralSystemAddress);
-        rewardLocker = ILnRewardLocker(_rewardLockerAddress);
+        collateralSystem = ICollateralSystem(_collateralSystemAddress);
+        rewardLocker = IRewardLocker(_rewardLockerAddress);
 
         // While we could in-theory calculate the EIP-712 domain separator off-chain, doing
         // it on-chain simplifies deployment and the cost here is one-off and acceptable.
@@ -92,7 +88,7 @@ contract MockRewardSystem is LnAdminUpgradeable {
         DOMAIN_SEPARATOR = keccak256(
             abi.encode(
                 keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"),
-                keccak256(bytes("Linear")),
+                keccak256(bytes("Athos")),
                 keccak256(bytes("1")),
                 chainId,
                 address(this)
@@ -100,20 +96,17 @@ contract MockRewardSystem is LnAdminUpgradeable {
         );
     }
 
-    function setRewardSigner(address _rewardSigner) external onlyAdmin {
+    function setRewardSigner(address _rewardSigner) external onlyOwner {
         _setRewardSigner(_rewardSigner);
     }
 
-    function setRewardLockerAddress(address _rewardLockerAddress) external onlyAdmin {
+    function setRewardLockerAddress(address _rewardLockerAddress) external onlyOwner {
         _setRewardLockerAddress(_rewardLockerAddress);
     }
 
-    function claimReward(
-        uint256 periodId,
-        uint256 stakingReward,
-        uint256 feeReward,
-        bytes calldata signature
-    ) external {
+    function claimReward(uint256 periodId, uint256 stakingReward, uint256 feeReward, bytes calldata signature)
+        external
+    {
         _claimReward(periodId, msg.sender, stakingReward, feeReward, signature);
     }
 
@@ -128,8 +121,8 @@ contract MockRewardSystem is LnAdminUpgradeable {
     }
 
     function _setRewardSigner(address _rewardSigner) private {
-        require(_rewardSigner != address(0), "LnRewardSystem: zero address");
-        require(_rewardSigner != rewardSigner, "LnRewardSystem: signer not changed");
+        require(_rewardSigner != address(0), "RewardSystem: zero address");
+        require(_rewardSigner != rewardSigner, "RewardSystem: signer not changed");
 
         address oldSigner = rewardSigner;
         rewardSigner = _rewardSigner;
@@ -138,11 +131,11 @@ contract MockRewardSystem is LnAdminUpgradeable {
     }
 
     function _setRewardLockerAddress(address _rewardLockerAddress) private {
-        require(_rewardLockerAddress != address(0), "LnRewardSystem: zero address");
-        require(_rewardLockerAddress != address(rewardLocker), "LnRewardSystem: address not changed");
+        require(_rewardLockerAddress != address(0), "RewardSystem: zero address");
+        require(_rewardLockerAddress != address(rewardLocker), "RewardSystem: address not changed");
 
         address oldAddress = address(rewardLocker);
-        rewardLocker = ILnRewardLocker(_rewardLockerAddress);
+        rewardLocker = IRewardLocker(_rewardLockerAddress);
 
         emit RewardLockerAddressChanged(oldAddress, address(rewardLocker));
     }
@@ -154,35 +147,34 @@ contract MockRewardSystem is LnAdminUpgradeable {
         uint256 feeReward,
         bytes calldata signature
     ) private {
-        require(periodId > 0, "LnRewardSystem: period ID must be positive");
-        require(stakingReward > 0 || feeReward > 0, "LnRewardSystem: nothing to claim");
+        require(periodId > 0, "RewardSystem: period ID must be positive");
+        require(stakingReward > 0 || feeReward > 0, "RewardSystem: nothing to claim");
 
         // Check if the target period is in the claiming window
         uint256 currentPeriodId = getCurrentPeriodId();
-        require(periodId < currentPeriodId, "LnRewardSystem: period not ended");
+        require(periodId < currentPeriodId, "RewardSystem: period not ended");
         require(
             currentPeriodId <= CLAIM_WINDOW_PERIOD_COUNT || periodId >= currentPeriodId - CLAIM_WINDOW_PERIOD_COUNT,
-            "LnRewardSystem: reward expired"
+            "RewardSystem: reward expired"
         );
 
         // Re-entrance prevention
-        require(userLastClaimPeriodIds[recipient] < periodId, "LnRewardSystem: reward already claimed");
+        require(userLastClaimPeriodIds[recipient] < periodId, "RewardSystem: reward already claimed");
         userLastClaimPeriodIds[recipient] = periodId;
 
         // Users can only claim rewards if target ratio is satisfied
-        require(collateralSystem.IsSatisfyTargetRatio(recipient), "LnRewardSystem: below target ratio");
+        require(collateralSystem.IsSatisfyTargetRatio(recipient), "RewardSystem: below target ratio");
 
         // Verify EIP-712 signature
-        bytes32 digest =
-            keccak256(
-                abi.encodePacked(
-                    "\x19\x01",
-                    DOMAIN_SEPARATOR,
-                    keccak256(abi.encode(REWARD_TYPEHASH, periodId, recipient, stakingReward, feeReward))
-                )
-            );
+        bytes32 digest = keccak256(
+            abi.encodePacked(
+                "\x19\x01",
+                DOMAIN_SEPARATOR,
+                keccak256(abi.encode(REWARD_TYPEHASH, periodId, recipient, stakingReward, feeReward))
+            )
+        );
         address recoveredAddress = digest.recover(signature);
-        require(recoveredAddress == rewardSigner, "LnRewardSystem: invalid signature");
+        require(recoveredAddress == rewardSigner, "RewardSystem: invalid signature");
 
         if (stakingReward > 0) {
             rewardLocker.addReward(recipient, stakingReward, getPeriodEndTime(periodId) + STAKING_REWARD_LOCK_PERIOD);
